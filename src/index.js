d9d43e031bd221feb8bf49472212df0a4fc42418
nasrin <emailer7po@gmail.com>
2022-04-29 19:25:24 +0430

    test

--- src/index.js
+++ src/index.js
@@ -91,3 +91,8 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+   
+
+
+
+
